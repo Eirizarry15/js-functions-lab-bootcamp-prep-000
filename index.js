b4307dd93a302f9e05e803ed4bb413a60fa2541f
Edgar Irizarry <eirizarry15@yahoo.com>
2018-09-10 21:13:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,3 @@ function happyholidays(){
   return "Happy Holidays!";
 } 
 
-function happyHolidaysto(){
-  console.log (`Happy Holidays, ${Zoe}!`)
-}
-
-function happyHollidayto(){
-  console.log (`Happy ${holiday}, ${name}!`)
-}
-
-function holidayCountdown(){
-  console.log (`It's ${days} days until ${holiday}!`)
-}
